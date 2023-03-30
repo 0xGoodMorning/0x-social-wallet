b4a839bb71c1c40ea96ac6eb66d7fe02f26e280c
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2023-03-30 18:19:45 +0300

    Add: send modal

--- src/pages/wallet.js
+++ src/pages/wallet.js
@@ -12,69 +12,108 @@ import {
   Input,
   useToast,
   VStack,
+  Modal,
+  useDisclosure,
+  ModalOverlay,
+  ModalContent,
+  Icon
 } from "@chakra-ui/react";
+import { CopyIcon } from '@chakra-ui/icons'
+import QRCode from "react-qr-code";
 
 export default function Wallet() {
-  const { account } = useAccount();
+  const { account } = useAccount()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const toast = useToast();
+
   return (
-    <VStack spacing={4} align="center">
-      <HStack spacing={4} alignItems="center" mb="2">
-        <Image
-          src="https://pbs.twimg.com/profile_images/804990434455887872/BG0Xh7Oa_400x400.jpg"
-          boxSize="100px"
-          borderRadius="full"
-          alt="Twitter image"
-        />
-        <VStack align="flex-start">
-          <Text fontSize="2xl" fontWeight="bold">
-            {account.name}
-          </Text>
-          <Text fontSize="l">
-            Twitter handle: <Link>@{account.handle}</Link>
-          </Text>
-        </VStack>
-      </HStack>
-      <VStack
-        w="full"
-        bg="gray.50"
-        borderWidth="1px"
-        borderRadius="md"
-        borderColor="gray.200"
-        p={6}
-        spacing={4}
-        alignItems="flex-start"
-      >
-        <HStack spacing={4} alignItems="center">
-          <Blockies width={50} height={50} seed={account.address} scale={8} />
+    <>
+      <VStack spacing={4} align="center">
+        <HStack spacing={4} alignItems="center" mb="2">
+          <Image
+            src="https://pbs.twimg.com/profile_images/804990434455887872/BG0Xh7Oa_400x400.jpg"
+            boxSize="100px"
+            borderRadius="full"
+            alt="Twitter image"
+          />
           <VStack align="flex-start">
-            <Text fontSize="xl" fontWeight="bold">
-              Crypto Address
+            <Text fontSize="2xl" fontWeight="bold">
+              {account.name}
             </Text>
-            <Text fontSize="md">{account.address}</Text>
+            <Text fontSize="l">
+              Twitter handle: <Link>@{account.handle}</Link>
+            </Text>
           </VStack>
         </HStack>
-        <Text fontSize="xl" fontWeight="bold">
-          Funds: ${account.balance}
-        </Text>
+        <VStack
+          w="full"
+          bg="gray.50"
+          borderWidth="1px"
+          borderRadius="md"
+          borderColor="gray.200"
+          p={6}
+          spacing={4}
+          alignItems="flex-start"
+        >
+          <HStack spacing={4} alignItems="center">
+            <Blockies width={50} height={50} seed={account.address} scale={8} />
+            <VStack align="flex-start">
+              <Text fontSize="xl" fontWeight="bold">
+                Crypto Address
+              </Text>
+              <Text fontSize="md">{account.address}</Text>
+            </VStack>
+          </HStack>
+          <Text fontSize="xl" fontWeight="bold">
+            Funds: ${account.balance}
+          </Text>
+        </VStack>
+        <HStack>
+          <Button
+            type="button"
+            colorScheme="blue"
+            bg="#1DA1F2"
+            _hover={{ bg: "blue.300", color: "white" }}
+            onClick={onOpen}
+          >
+            Send
+          </Button>
+          <Button
+            type="button"
+            colorScheme="blue"
+            bg="#1DA1F2"
+            _hover={{ bg: "blue.300", color: "white" }}
+          >
+            Claim with Twitter
+          </Button>
+        </HStack>
       </VStack>
-      <HStack>
-        <Button
-          type="button"
-          colorScheme="blue"
-          bg="#1DA1F2"
-          _hover={{ bg: "blue.300", color: "white" }}
-        >
-          Send
-        </Button>
-        <Button
-          type="button"
-          colorScheme="blue"
-          bg="#1DA1F2"
-          _hover={{ bg: "blue.300", color: "white" }}
-        >
-          Claim with Twitter
-        </Button>
-      </HStack>
-    </VStack>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered size='2xl'>
+        <ModalOverlay />
+          <ModalContent padding={16}>
+            <VStack align="center">
+            <QRCode
+              size={256}
+              style={{ marginBottom: 10 }}
+              value={account.address || ''}
+              viewBox={`0 0 256 256`}
+              />
+              <HStack>
+                <Text fontSize="sm" noOfLines={1}>{account.address}</Text>
+                <CopyIcon onClick={() => {
+                  navigator.clipboard.writeText(account.address)
+                  toast({
+                    title: "Success",
+                    description: `Address copied to clipboard`,
+                    status: "success",
+                    duration: 750,
+                    isClosable: false,
+                  });
+                  }} />
+              </HStack>
+            </VStack>
+          </ModalContent>
+      </Modal>
+    </>
   );
 }
